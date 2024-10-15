b82522dcfce90e60d730ad0b0f668d1beb16036a
numank <numank@Ubuntu.myguest.virtualbox.org>
2024-10-15 14:25:17 +0300

    dfgdfgdf

--- src/tttdeneme/Main.java
+++ src/tttdeneme/Main.java
@@ -12,6 +12,7 @@ public class Main
 		System.err.println("Task 1 deneme");
 		System.err.println("Task 2");
 		System.err.println("master");
+		// #task2_ex
 		System.err.println("master 2");
 	}
 }
